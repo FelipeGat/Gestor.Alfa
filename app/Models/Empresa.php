1e22ebaa94c8f7eb228232470a006f6d5eda749f
Felipe <felipehenriquegat@gmail.comno>
2026-01-08 10:22:51 -0300

    Criação do Model Empresas

---
+++ app/Models/Empresa.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Cliente;
+use App\Models\Funcionario;
+
+class Empresa extends Model
+{
+    use SoftDeletes;
+    
+    protected $table = 'empresas';
+
+    protected $fillable = [
+        'razao_social',
+        'nome_fantasia',
+        'cnpj',
+        'endereco',
+        'email_comercial',
+        'email_administrativo',
+        'telefone_comercial',
+        'ativo',
+    ];
+
+    protected $casts = [
+        'ativo' => 'boolean',
+    ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relacionamentos
+    |--------------------------------------------------------------------------
+    */
+
+    public function clientes()
+    {
+        return $this->belongsToMany(Cliente::class);
+    }
+
+    public function funcionarios()
+    {
+        return $this->belongsToMany(Funcionario::class);
+    }
+}
