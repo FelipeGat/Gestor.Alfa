fd1908b854714ee141dd2c93c6ff18495970ace0
Rossini Santos <rossinisantos@gmail.com>
2026-02-17 08:31:31 -0300

    fix: remover emoji do título da página de edição de empresa

--- resources/views/empresas/edit.blade.php
+++ resources/views/empresas/edit.blade.php
@@ -6,7 +6,7 @@
 
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            ✏️ Editar Empresa
+            Editar Empresa
         </h2>
     </x-slot>
 
