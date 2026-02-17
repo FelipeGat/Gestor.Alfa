99e0c3f5a3d298a878d216b09942391f4c85ab9a
rossini06 <rossinidesouza06@gmail.com>
2026-02-17 01:40:46 +0000

    fix: padronizar botões de atualizar em usuários e fornecedores

--- resources/views/usuarios/edit.blade.php
+++ resources/views/usuarios/edit.blade.php
@@ -91,26 +91,13 @@
                 </div>
 
                 <div class="mt-6 flex justify-end gap-3">
-                    <a href="{{ route('usuarios.index') }}"
-                        class="btn btn-cancelar inline-flex items-center justify-center px-6 py-2 rounded-lg border border-gray-300 text-gray-700 font-medium hover:bg-gray-50 transition duration-200">
-
-                        <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd"
-                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                clip-rule="evenodd" />
-                        </svg>
-
+                    <x-secondary-button href="{{ route('usuarios.index') }}">
                         Cancelar
-                    </a>
+                    </x-secondary-button>
 
-                    <button type="submit" class="btn btn-primary">
-                        <svg fill="currentColor" viewBox="0 0 20 20">
-                            <path fill-rule="evenodd"
-                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                clip-rule="evenodd" />
-                        </svg>
-                        Atualizar Funcionario
-                    </button>
+                    <x-primary-button>
+                        Atualizar Usuário
+                    </x-primary-button>
                 </div>
 
             </form>
