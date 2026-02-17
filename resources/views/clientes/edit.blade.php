db9c748e6dd3831cd1b391d7d2a50e8ea909e850
Rossini Santos <rossinisantos@gmail.com>
2026-02-17 09:28:57 -0300

    fix: usar ícones de envelope e telefone nos botões adicionar

--- resources/views/clientes/edit.blade.php
+++ resources/views/clientes/edit.blade.php
@@ -361,10 +361,9 @@
                                         class="text-red-500">*</span></label>
                                 <button type="button" onclick="addEmail()"
                                     class="btn btn-success" style="padding: 0.5rem 1rem; font-size: 0.875rem; line-height: 1.25rem; min-width: 130px; justify-content: center; background: #22c55e; border-radius: 9999px;">
-                                    <svg fill="currentColor" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd"
-                                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z"
-                                            clip-rule="evenodd" />
+                                    <svg fill="currentColor" viewBox="0 0 20 20" style="width: 18px; height: 18px;">
+                                        <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"/>
+                                        <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"/>
                                     </svg>
                                     Adicionar
                                 </button>
@@ -391,10 +390,8 @@
                                 <label class="block text-sm font-medium text-gray-700">Telefones</label>
                                 <button type="button" onclick="addTelefone()"
                                     class="btn btn-success" style="padding: 0.5rem 1rem; font-size: 0.875rem; line-height: 1.25rem; min-width: 130px; justify-content: center; background: #22c55e; border-radius: 9999px;">
-                                    <svg fill="currentColor" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd"
-                                            d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z"
-                                            clip-rule="evenodd" />
+                                    <svg fill="currentColor" viewBox="0 0 20 20" style="width: 18px; height: 18px;">
+                                        <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"/>
                                     </svg>
                                     Adicionar
                                 </button>
