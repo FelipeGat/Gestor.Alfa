a4493390e0b1d4330660339a472849586ae754e5
Usuario Exemplo <usuario@exemplo.com>
2026-02-18 07:35:17 -0300

    fix: corrigir breadcrumb de Custos x Orçamentos para Relatórios

--- resources/views/relatorios/custos-orcamentos/index.blade.php
+++ resources/views/relatorios/custos-orcamentos/index.blade.php
@@ -18,7 +18,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
             </svg>
-            <a href="{{ route('financeiro.dashboard') }}" class="text-gray-500 hover:text-gray-700 transition">Dashboard Financeiro</a>
+            <a href="{{ route('relatorios.index') }}" class="text-gray-500 hover:text-gray-700 transition">Relatórios</a>
             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
             </svg>
