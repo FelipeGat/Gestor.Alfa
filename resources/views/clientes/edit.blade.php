0011d11c7c20db7d0f4a4319d7d45fa4a76e9db4
Rossini Santos <rossinisantos@gmail.com>
2026-02-21 06:44:59 -0300

    style: padroniza largura das páginas de criar e editar clientes

--- resources/views/clientes/edit.blade.php
+++ resources/views/clientes/edit.blade.php
@@ -297,7 +297,7 @@
     </script>
 
     <div class="pb-8">
-        <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 pt-4">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-4">
 
 
 
