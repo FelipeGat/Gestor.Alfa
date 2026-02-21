dfd3771f55fe1dfd0799132c5b4b92b9d58a12b3
Rossini Santos <rossinisantos@gmail.com>
2026-02-21 06:54:04 -0300

    style: padroniza largura das páginas de criar e editar funcionários

--- resources/views/funcionarios/create.blade.php
+++ resources/views/funcionarios/create.blade.php
@@ -47,7 +47,7 @@
     </x-slot>
 
     <div class="pb-8">
-        <div class="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
             {{-- ERROS --}}
             @if ($errors->any())
