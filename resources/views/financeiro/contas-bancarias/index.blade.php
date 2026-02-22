b075c9fbf3ed90fd6644d4f0e7efddd69b96ef8c
Rossini Santos <rossinisantos@gmail.com>
2026-02-22 15:25:47 -0300

    fix: padroniza botões de ação da página de Bancos com o mesmo estilo das outras páginas

--- resources/views/financeiro/contas-bancarias/index.blade.php
+++ resources/views/financeiro/contas-bancarias/index.blade.php
@@ -233,8 +233,8 @@
                                 </td>
                                 <td class="px-4 py-3 text-left">
                                     <div class="flex gap-1 items-center justify-start">
-                                        <a href="{{ route('financeiro.contas-financeiras.edit', $conta) }}" class="inline-flex items-center justify-center w-8 h-8 bg-blue-600 hover:bg-blue-700 text-white rounded-full transition" title="Editar">
-                                            <svg fill="currentColor" viewBox="0 0 20 20" style="width: 18px; height: 18px;">
+                                        <a href="{{ route('financeiro.contas-financeiras.edit', $conta) }}" class="btn btn-edit" title="Editar">
+                                            <svg fill="currentColor" viewBox="0 0 20 20">
                                                 <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
                                             </svg>
                                         </a>
@@ -244,15 +244,15 @@
                                             onsubmit="return confirm('Deseja remover esta conta?')">
                                             @csrf
                                             @method('DELETE')
-                                            <button type="submit" class="inline-flex items-center justify-center w-8 h-8 bg-red-600 hover:bg-red-700 text-white rounded-full transition" title="Excluir">
-                                                <svg fill="currentColor" viewBox="0 0 20 20" style="width: 18px; height: 18px;">
+                                            <button type="submit" class="btn btn-delete" title="Excluir">
+                                                <svg fill="currentColor" viewBox="0 0 20 20">
                                                     <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
                                                 </svg>
                                             </button>
                                         </form>
 
-                                        <button type="button" class="inline-flex items-center justify-center w-8 h-8 bg-yellow-500 hover:bg-yellow-600 text-white rounded-full transition" title="Ajuste Manual" onclick="abrirModalAjusteUnificado({{ $conta->id }}, '{{ $conta->nome }}')">
-                                            <svg fill="currentColor" viewBox="0 0 20 20" style="width: 18px; height: 18px;">
+                                        <button type="button" class="btn btn-adjust" title="Ajuste Manual" onclick="abrirModalAjusteUnificado({{ $conta->id }}, '{{ $conta->nome }}')">
+                                            <svg fill="currentColor" viewBox="0 0 20 20">
                                                 <path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
                                             </svg>
                                         </button>
