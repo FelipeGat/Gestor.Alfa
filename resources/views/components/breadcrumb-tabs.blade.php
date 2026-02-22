5fd6a5aa670922415efa3f351f03eb74a7a6e7ab
Rossini Santos <rossinisantos@gmail.com>
2026-02-22 11:55:46 -0300

    feat: adiciona borda inferior nas abas do breadcrumb

--- resources/views/components/breadcrumb-tabs.blade.php
+++ resources/views/components/breadcrumb-tabs.blade.php
@@ -10,13 +10,13 @@
             @if (!$isLast && isset($item['url']))
                 <a href="{{ $item['url'] }}"
                    class="relative bg-gray-200 px-4 py-2 text-sm font-semibold text-gray-600
-                          rounded-t-lg border border-gray-300 border-b-0
+                          rounded-t-lg border border-gray-300
                           hover:bg-gray-300 hover:text-gray-800 transition-all">
                     {{ $item['label'] }}
                 </a>
             @else
                 <span class="relative bg-white px-4 py-2 text-sm font-semibold text-[#3f9cae]
-                             rounded-t-lg border-2 border-[#3f9cae] border-b-0">
+                             rounded-t-lg border-2 border-[#3f9cae]">
                     {{ $item['label'] }}
                 </span>
             @endif
