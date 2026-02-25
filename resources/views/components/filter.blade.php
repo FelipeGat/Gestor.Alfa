69f72d0978e258764adba740bdc04401348d5aa5
Rossini Santos <rossinisantos@gmail.com>
2026-02-25 12:48:56 -0300

    fix: usa prop iconLeft para exibir ícones corretamente nos botões de filtro

--- resources/views/components/filter.blade.php
+++ resources/views/components/filter.blade.php
@@ -23,18 +23,14 @@
         
         {{-- Botões de ação --}}
         <div class="flex items-end gap-2">
-            <x-button type="submit" variant="primary" size="sm" class="min-w-[80px]">
-                <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" />
-                </svg>
+            <x-button type="submit" variant="primary" size="sm" class="min-w-[80px]"
+                iconLeft='<svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M3 3a1 1 0 011-1h12a1 1 0 011 1v3a1 1 0 01-.293.707L12 11.414V15a1 1 0 01-.293.707l-2 2A1 1 0 018 17v-5.586L3.293 6.707A1 1 0 013 6V3z" clip-rule="evenodd" /></svg>'>
                 Filtrar
             </x-button>
             
             @if($showClearButton)
-                <x-button href="{{ $action }}" variant="secondary" size="sm" class="min-w-[80px]">
-                    <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
-                    </svg>
+                <x-button href="{{ $action }}" variant="secondary" size="sm" class="min-w-[80px]"
+                    iconLeft='<svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>'>
                     Limpar
                 </x-button>
             @endif
