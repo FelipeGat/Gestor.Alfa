9a5d5586e6442c09e9fbe288aef150417db5e374
Rossini Santos <rossinisantos@gmail.com>
2026-02-27 16:04:59 -0300

    fix(contas-receber): padroniza botoes de acao seguindo padrao do sistema

--- resources/views/financeiro/contasareceber.blade.php
+++ resources/views/financeiro/contasareceber.blade.php
@@ -347,10 +347,10 @@
                                         <button
                                             type="button"
                                             x-data
-                                            class="btn action-btn btn-icon bg-blue-600 hover:bg-blue-700 text-white transition-transform duration-200 hover:scale-150"
+                                            class="p-2 rounded-full inline-flex items-center justify-center text-[#3f9cae] hover:bg-[#3f9cae]/10 transition"
                                             title="Editar Conta Fixa"
                                             @click="$dispatch('editar-conta-fixa', { contaFixaId: {{ $cobranca->conta_fixa_id }} })">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 20 20" fill="currentColor">
                                                 <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
                                             </svg>
                                         </button>
@@ -361,7 +361,7 @@
                                         <button
                                             type="button"
                                             x-data
-                                            class="btn action-btn btn-icon bg-green-600 hover:bg-green-700 text-white transition-transform duration-200 hover:scale-150"
+                                            class="p-2 rounded-full inline-flex items-center justify-center text-green-600 hover:bg-green-50 transition"
                                             title="Confirmar Baixa"
                                             x-on:click="$dispatch('confirmar-baixa', {
     action: '{{ route('financeiro.contasareceber.pagar', $cobranca) }}',
@@ -370,7 +370,7 @@
     valorTotal: {{ $cobranca->valor }},
     dataVencimento: '{{ $cobranca->data_vencimento->format('Y-m-d') }}'
 })">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 20 20" fill="currentColor">
                                                 <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
                                                 <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clip-rule="evenodd" />
                                             </svg>
@@ -381,14 +381,14 @@
                                         <button
                                             type="button"
                                             x-data
-                                            class="btn action-btn btn-icon bg-gray-600 hover:bg-gray-700 text-white transition-transform duration-200 hover:scale-150 relative"
+                                            class="p-2 rounded-full inline-flex items-center justify-center text-blue-600 hover:bg-blue-50 transition relative"
                                             title="Gerenciar Anexos (NF/Boleto)"
                                             @click="$dispatch('abrir-modal-anexos', { cobrancaId: {{ $cobranca->id }} })">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 20 20" fill="currentColor">
                                                 <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
                                             </svg>
                                             @if($cobranca->anexos()->count() > 0)
-                                            <span class="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+                                            <span class="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-4 w-4 flex items-center justify-center">
                                                 {{ $cobranca->anexos()->count() }}
                                             </span>
                                             @endif
@@ -412,7 +412,7 @@
                                         <button
                                             type="button"
                                             x-data
-                                            class="btn action-btn btn-icon bg-red-400 hover:bg-red-500 text-white transition-transform duration-200 hover:scale-150"
+                                            class="p-2 rounded-full inline-flex items-center justify-center text-red-600 hover:bg-red-50 transition"
                                             title="Excluir"
                                             @click="$dispatch('excluir-cobranca', {
                                                 cobrancaId: {{ $cobranca->id }},
@@ -422,7 +422,7 @@
                                                 totalCobrancasContrato: {{ $totalCobrancasContrato }},
                                                 dataVencimento: '{{ $cobranca->data_vencimento }}'
                                             })">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 20 20" fill="currentColor">
                                                 <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
                                             </svg>
                                         </button>
