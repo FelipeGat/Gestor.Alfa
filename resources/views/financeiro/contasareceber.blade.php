dd8223c5edfc7f6aecfc9022c780a990d2810f18
Rossini Santos <rossinisantos@gmail.com>
2026-02-27 22:19:38 -0300

    fix(contas-receber): altera cor do badge de anexos para azul

--- resources/views/financeiro/contasareceber.blade.php
+++ resources/views/financeiro/contasareceber.blade.php
@@ -390,7 +390,7 @@
                                                 <path fill-rule="evenodd" d="M8 4a3 3 0 00-3 3v4a5 5 0 0010 0V7a1 1 0 112 0v4a7 7 0 11-14 0V7a5 5 0 0110 0v4a3 3 0 11-6 0V7a1 1 0 012 0v4a1 1 0 102 0V7a3 3 0 00-3-3z" clip-rule="evenodd" />
                                             </svg>
                                             @if($cobranca->anexos()->count() > 0)
-                                            <span class="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full h-4 w-4 flex items-center justify-center">
+                                            <span class="absolute -top-1 -right-1 bg-blue-600 text-white text-xs rounded-full h-4 w-4 flex items-center justify-center">
                                                 {{ $cobranca->anexos()->count() }}
                                             </span>
                                             @endif
