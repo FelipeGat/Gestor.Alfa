8f9314f86d6e440b62a870efca0d9a87df8f5694
rossini06 <rossinidesouza06@gmail.com>
2026-03-10 19:13:26 +0000

    feat(notifications): implementar NotificacaoService e integração com AtendimentoController

---
+++ app/Services/NotificacaoService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use Kreait\Laravel\Facades\FirebaseMessaging;
+use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Firebase\Messaging\Notification;
+
+class NotificacaoService
+{
+    /**
+     * Envia uma notificação para um usuário específico
+     *
+     * @param User $user
+     * @param string $titulo
+     * @param string $corpo
+     * @param array $dados Adicionais (tipo, id_chamado, etc)
+     * @return bool
+     */
+    public function enviarParaUsuario(User $user, string $titulo, string $corpo, array $dados = []): bool
+    {
+        if (!$user->fcm_token) {
+            return false;
+        }
+
+        try {
+            $notification = Notification::create($titulo, $corpo);
+            $message = CloudMessage::withTarget('token', $user->fcm_token)
+                ->withNotification($notification)
+                ->withData($dados);
+
+            FirebaseMessaging::send($message);
+            return true;
+        } catch (\Exception $e) {
+            \Log::error("Erro ao enviar notificação FCM para o usuário {$user->id}: " . $e->getMessage());
+            return false;
+        }
+    }
+}
